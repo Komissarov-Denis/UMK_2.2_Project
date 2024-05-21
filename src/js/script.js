0dc84fb07952b29b6a26777fe0d4973767f1be27
Denis <KILLDEN@RAMBLER.RU>
2024-05-21 19:10:03 +0300

    Update 21.05.24

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.header_nav_menu');
+  const menuItem = document.querySelectorAll('.header_nav_menu_item');
+  const hamburger = document.querySelector('.header_nav_block_hamburger');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('header_nav_block_hamburger_active');
+    menu.classList.toggle('header_nav_menu_active');
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('header_nav_block_hamburger_active');
+      menu.classList.toggle('header_nav_menu_active');
+    });
+  });
+});
