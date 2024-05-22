6ef0d39a44dc486f5996a1afd5ddfb688cf954aa
Denis <KILLDEN@RAMBLER.RU>
2024-05-22 12:46:13 +0300

    Update 22.05.24

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const menu = document.querySelector('.header_nav_menu');
-  const menuItem = document.querySelectorAll('.header_nav_menu_item');
-  const hamburger = document.querySelector('.header_nav_block_hamburger');
 
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('header_nav_block_hamburger_active');
-    menu.classList.toggle('header_nav_menu_active');
-  });
 
-  menuItem.forEach((item) => {
-    item.addEventListener('click', () => {
-      hamburger.classList.toggle('header_nav_block_hamburger_active');
-      menu.classList.toggle('header_nav_menu_active');
-    });
-  });
+	// hamburger------------------------------------------------
+	// const menu = document.querySelector('.header_nav_menu');
+	// const menuItem = document.querySelectorAll('.header_nav_menu_item');
+	// const hamburger = document.querySelector('.header_nav_block_hamburger');
+
+	// hamburger.addEventListener('click', () => {
+	// 	hamburger.classList.toggle('header_nav_block_hamburger_active');
+	// 	menu.classList.toggle('header_nav_menu_active');
+	// });
+
+	// menuItem.forEach((item) => {
+	// 	item.addEventListener('click', () => {
+	// 		hamburger.classList.toggle('header_nav_block_hamburger_active');
+	// 		menu.classList.toggle('header_nav_menu_active');
+	// 	});
+	// });
+
+	// modal----------------------------------------------------
+	function openModalWindow(modalSelector) {
+
+	}
+	openModalWindow();
+
+
+
 });
