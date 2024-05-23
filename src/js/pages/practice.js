ca98850f08446a348542ff0a640ce75b693071f1
Denis <KILLDEN@RAMBLER.RU>
2024-05-23 14:39:00 +0300

    Update 23.05.24

--- src/js/pages/practice.js
+++ src/js/pages/practice.js
@@ -7,4 +7,19 @@ window.addEventListener('DOMContentLoaded', () => {
 		console.log('click');
 	});
 
+	// hamburger--------------------------------------------
+	function hamburger() {
+		const hamburger = document.querySelector('.hamburger'),
+			menu = document.querySelector('.menu'),
+			closeElem = document.querySelector('.menu__close');
+		
+		hamburger.addEventListener('click', () => {
+			menu.classList.add('menu_active');
+		});
+		closeElem.addEventListener('click', () => {
+			menu.classList.remove('menu_active');
+		});
+	}
+	hamburger();
+
 });
--- src/js/pages/tests.js
+++ src/js/pages/tests.js
@@ -7,4 +7,34 @@ window.addEventListener('DOMContentLoaded', () => {
 		console.log('click');
 	});
 
+	// hamburger--------------------------------------------
+	function hamburger() {
+		const hamburger = document.querySelector('.hamburger'),
+			span = document.querySelectorAll('.hamburger__span-line'),
+			menu = document.querySelector('.menu'),
+			closeElem = document.querySelector('.menu__close');
+		console.log(span);
+		hamburger.addEventListener('click', () => {
+			menu.classList.add('menu_active');
+			span.forEach(function(item) {
+				item.style.border = '2px';
+				item.style.borderStyle = 'solid';
+				item.style.borderColor = 'rgb(19, 54, 118)';
+				item.style.background = 'rgb(217, 220, 236)';
+				console.log(item);				
+			});			
+		});		
+		closeElem.addEventListener('click', () => {
+			menu.classList.remove('menu_active');
+			span.forEach(function(item) {
+				item.style.border = 'none';
+				item.style.borderStyle = 'none';
+				item.style.borderColor = 'none';
+				item.style.background = 'rgb(19, 54, 118)';
+				console.log(item);				
+			});			
+		});
+	}
+	hamburger();
+
 });
