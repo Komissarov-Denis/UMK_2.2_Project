ed49476231a054dc9541084bb9524ea1d97aa1b4
Denis <KILLDEN@RAMBLER.RU>
2024-05-28 17:35:11 +0300

    Update 28.05.24

--- src/js/pages/subtheory/config.js
+++ src/js/pages/subtheory/config.js
@@ -1,12 +1,95 @@
 window.addEventListener('DOMContentLoaded', () => {
 
 	// to-carousel1-------------------------------------------------
-	const btnConfig = document.querySelector('.btn_open-carousel1');
-	btnConfig.addEventListener('click', function() {
+	const btnConfig1 = document.querySelector('.btn_open-carousel1');
+	btnConfig1.addEventListener('click', function() {
 		window.location.href = '../../carousel/theory-pages/carousel1.html';
 		// console.log('click');
 	});
-
+	// to-carousel2-------------------------------------------------
+	const btnConfig2 = document.querySelector('.btn_open-carousel2');
+	btnConfig2.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel2.html';
+		// console.log('click');
+	});
+	// to-carousel3-------------------------------------------------
+	const btnConfig3 = document.querySelector('.btn_open-carousel3');
+	btnConfig3.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel3.html';
+		// console.log('click');
+	});
+	// to-carousel4-------------------------------------------------
+	const btnConfig4 = document.querySelector('.btn_open-carousel4');
+	btnConfig4.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel4.html';
+		// console.log('click');
+	});
+	// to-carousel5-------------------------------------------------
+	const btnConfig5 = document.querySelector('.btn_open-carousel5');
+	btnConfig5.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel5.html';
+		// console.log('click');
+	});
+	// to-carousel6-------------------------------------------------
+	const btnConfig6 = document.querySelector('.btn_open-carousel6');
+	btnConfig6.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel6.html';
+		// console.log('click');
+	});
+	// to-carousel7-------------------------------------------------
+	const btnConfig7 = document.querySelector('.btn_open-carousel7');
+	btnConfig7.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel7.html';
+		// console.log('click');
+	});
+	// to-carousel8-------------------------------------------------
+	const btnConfig8 = document.querySelector('.btn_open-carousel8');
+	btnConfig8.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel8.html';
+		// console.log('click');
+	});
+	// to-carousel9-------------------------------------------------
+	const btnConfig9 = document.querySelector('.btn_open-carousel9');
+	btnConfig9.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel9.html';
+		// console.log('click');
+	});
+	// to-carousel10-------------------------------------------------
+	const btnConfig10 = document.querySelector('.btn_open-carousel10');
+	btnConfig10.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel10.html';
+		// console.log('click');
+	});
+	// to-carousel11-------------------------------------------------
+	const btnConfig11 = document.querySelector('.btn_open-carousel11');
+	btnConfig11.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel11.html';
+		// console.log('click');
+	});
+	// to-carousel12-------------------------------------------------
+	const btnConfig12 = document.querySelector('.btn_open-carousel12');
+	btnConfig12.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel12.html';
+		// console.log('click');
+	});
+	// to-carousel13-------------------------------------------------
+	const btnConfig13 = document.querySelector('.btn_open-carousel13');
+	btnConfig13.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel13.html';
+		// console.log('click');
+	});
+	// to-carousel14-------------------------------------------------
+	const btnConfig14 = document.querySelector('.btn_open-carousel14');
+	btnConfig14.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel14.html';
+		// console.log('click');
+	});
+	// to-carousel15-------------------------------------------------
+	const btnConfig15 = document.querySelector('.btn_open-carousel15');
+	btnConfig15.addEventListener('click', function() {
+		window.location.href = '../../carousel/theory-pages/carousel15.html';
+		// console.log('click');
+	});
 	
 
 	// hamburger--------------------------------------------
